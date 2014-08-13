54296d6424accde7b7b5d34d0a4124f2610395ca
Stuart Rackham <srackham@gmail.com>
2014-08-14 11:37:20 +1200

    Explicitly reference shelljs methods instead of importing globally.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
 
-  require('shelljs/global');
+  var shelljs = require('shelljs');
 
   /* Inputs and outputs */
 
   // The source file compilation order is important.
-  SOURCE = [
+  var SOURCE = [
     'src/rimu.ts',
     'src/helpers.ts',
     'src/options.ts',
@@ -19,15 +19,15 @@ module.exports = function(grunt) {
     'src/replacements.ts'
   ];
 
-  TESTS = ls('test/*.js');
+  var TESTS = shelljs.ls('test/*.js');
 
-  DOCS = [
+  var DOCS = [
     {src: 'README.md', dst: 'doc/index.html', title: 'Rimu Markup'},
     {src: 'doc/tips.rmu', dst: 'doc/tips.html', title: 'Rimu Tips'},
     {src: 'doc/showcase.rmu', dst: 'doc/showcase.html', title: 'Rimu Showcase'}
   ];
 
-  PKG = grunt.file.readJSON('package.json');
+  var PKG = grunt.file.readJSON('package.json');
 
 
   /* Tasks */
@@ -35,30 +35,30 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['compile', 'lint', 'uglify', 'test', 'docs']);
 
   grunt.registerTask('lint', 'Lint Javascript and JSON files', function() {
-    exec('jshint ' + TESTS.join(' ') + ' bin/rimuc.js');
-    exec('jsonlint --quiet package.json');
+    shelljs.exec('jshint ' + TESTS.join(' ') + ' bin/rimuc.js');
+    shelljs.exec('jsonlint --quiet package.json');
   });
 
   grunt.registerTask('compile', 'Compile Typescript to JavaScript then uglify', function() {
-    exec('tsc --declaration --out bin/rimu.js ' + SOURCE.join(' '));
+    shelljs.exec('tsc --declaration --out bin/rimu.js ' + SOURCE.join(' '));
   });
 
   grunt.registerTask('uglify', 'Minimize compiled JavaScript', function() {
     var preamble = '/* ' + PKG.name + ' ' + PKG.version + ' (' + PKG.repository.url + ') */';
-    exec('uglifyjs --preamble "' + preamble + '" bin/rimu.js', {silent: true})
+    shelljs.exec('uglifyjs --preamble "' + preamble + '" bin/rimu.js', {silent: true})
         .output
         .to('bin/rimu.min.js');
   });
 
   grunt.registerTask('test', 'Run unit tests', function() {
     TESTS.forEach(function(file) {
-      exec('nodeunit ' + file);
+      shelljs.exec('nodeunit ' + file);
     });
   });
 
   grunt.registerTask('docs', 'Generate HTML documentation', function() {
     DOCS.forEach(function(doc) {
-      exec('node ./bin/rimuc.js --output ' + doc.dst
+      shelljs.exec('node ./bin/rimuc.js --output ' + doc.dst
               + ' --prepend "{--title}=\'' + doc.title + '\'"'
               + ' doc/doc-header.rmu ' + doc.src + ' doc/doc-footer.rmu'
       )
@@ -68,14 +68,14 @@ module.exports = function(grunt) {
   grunt.registerTask('version', 'Display or update (--package-version option) the project version number', function() {
     var version = grunt.option('package-version');
     if (!version) {
-      echo('\nversion: ' + PKG.version);
+      shelljs.echo('\nversion: ' + PKG.version);
     }
     else {
       if (!version.match(/^\d+\.\d+\.\d+$/)) {
         grunt.warn('Invalid version number: ' + version + '\n');
       }
-      ['package.json','smart.json'].forEach(function(file) {
-        sed('-i', /(\n\s*"version"\s*:\s*)"\d+\.\d+\.\d+"/, '$1' + '"' + version + '"', file);
+      ['package.json', 'smart.json'].forEach(function(file) {
+        shelljs.sed('-i', /(\n\s*"version"\s*:\s*)"\d+\.\d+\.\d+"/, '$1' + '"' + version + '"', file);
       });
     }
   });
@@ -85,21 +85,21 @@ module.exports = function(grunt) {
     if (!commit_message) {
       grunt.warn('Missing command-line option: -m "commit-message"\n');
     }
-    exec('git commit -a -m "' + commit_message + '"');
+    shelljs.exec('git commit -a -m "' + commit_message + '"');
   });
 
   grunt.registerTask('push', 'Push local commits to Github', function() {
-    exec('git push -u --tags origin master');
+    shelljs.exec('git push -u --tags origin master');
   });
 
   grunt.registerTask('publish', ['push', 'publish-npm', 'publish-meteor']);
 
   grunt.registerTask('publish-npm', 'Publish to npm', function() {
-    exec('npm publish');
+    shelljs.exec('npm publish');
   });
 
   grunt.registerTask('publish-meteor', 'Publish to Meteor', function() {
-    exec('mrt publish');
+    shelljs.exec('mrt publish');
   });
 
 };
