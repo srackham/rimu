87e1df7382ca1e7674f6747a133152b4342828a1
Stuart Rackham <srackham@gmail.com>
2015-01-23 18:06:57 +1300

    Rebuilt project website.

--- rimu.js
+++ rimu.js
@@ -1305,6 +1305,17 @@ var Rimu;
   Dependent source file references.
   The order is important as it defines the order in which the files are compiled to the output file.
  */
+/// <reference path="helpers.ts" />
+/// <reference path="main.ts" />
+/// <reference path="options.ts" />
+/// <reference path="io.ts" />
+/// <reference path="macros.ts" />
+/// <reference path="lineblocks.ts" />
+/// <reference path="delimitedblocks.ts" />
+/// <reference path="lists.ts" />
+/// <reference path="spans.ts" />
+/// <reference path="quotes.ts" />
+/// <reference path="replacements.ts" />
 /// <reference path="references.ts" />
 /**
  * ### Rimu Modularization
