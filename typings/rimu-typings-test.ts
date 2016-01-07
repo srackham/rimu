ac168ddf0fb037fcf591a9d1865a6dd448dc94c0
Stuart Rackham <srackham@gmail.com>
2016-01-08 08:30:02 +1300

    docs: Usage comments in rimu example app.

--- typings/rimu-typings-test.ts
+++ typings/rimu-typings-test.ts
@@ -1,12 +1,20 @@
 /*
- Simplest test for rimu.d.ts
+ Simplest TypeScript application.
 
  tsc --module commonjs rimu-typings-test.ts   # Compile.
  node rimu-typings-test.js                    # Run.
 
  */
 
+// Use this in a nodejs application.
+// Compiles using rimu.d.ts typings from installed rimu npm package.
+
 import Rimu = require('rimu');
+
+// Use this in a generic application.
+// Compiles using rimu.d.ts typings located in current directory.
+
+// import Rimu = require('./rimu');
 
 console.log(Rimu.render('Hello *Rimu*!', {safeMode:1}));
 
