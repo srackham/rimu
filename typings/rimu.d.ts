8a5b0996be3bcfd3438389cde3371d63b7e3383a
Stuart Rackham <srackham@gmail.com>
2016-01-08 08:07:01 +1300

    fix: Make rimu.d.ts compatible with TypeScript package.json `typings` feature (http://stackoverflow.com/questions/34623590/typescript-npm-typings-feature-changes-import-semantics).

--- typings/rimu.d.ts
+++ typings/rimu.d.ts
@@ -1,18 +1,16 @@
 // Type definitions for Rimu Markup
 // Project: https://github.com/srackham/rimu
 
-declare module 'rimu' {
-  export interface Options {
-    safeMode?: number;
-    htmlReplacement?: string;
-    macroMode?: number;
-    reset?: boolean;
-    callback?: CallbackFunction;
-  }
-  export type CallbackFunction = (message: CallbackMessage) => void;
-  export interface CallbackMessage {
-    type: string;
-    text: string;
-  }
-  export function render(source: string, options?: Options): string;
+export interface Options {
+  safeMode?: number;
+  htmlReplacement?: string;
+  macroMode?: number;
+  reset?: boolean;
+  callback?: CallbackFunction;
 }
+export type CallbackFunction = (message: CallbackMessage) => void;
+export interface CallbackMessage {
+  type: string;
+  text: string;
+}
+export function render(source: string, options?: Options): string;
