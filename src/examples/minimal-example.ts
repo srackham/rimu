c4a7f4df62a8460dd0bdd7dd7bf253c253094d77
Stuart Rackham <srackham@gmail.com>
2017-04-07 10:23:46 +1200

    docs: minimal-example.ts comments.

--- src/examples/minimal-example.ts
+++ src/examples/minimal-example.ts
@@ -1,13 +1,14 @@
 /*
  Simplest Rimu TypeScript application.
 
- tsc --module commonjs minimal-example.ts   # Compile.
- node minimal-example.js                    # Run.
+ npm install rimu                           # Install Rimu module.
+ tsc --module commonjs minimal-example.ts   # Compile example.
+ node minimal-example.js                    # Run example.
 
  */
 
 // Use this in a nodejs application.
-// Compiles using rimu.d.ts typings from installed rimu npm package.
+// Compiles using rimu.d.ts typings from rimu npm package.
 
 import Rimu = require('rimu');
 
