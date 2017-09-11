e2208ae63dd5b5dd22630aba4e89dc358d320dbb
Stuart Rackham <srackham@gmail.com>
2017-09-11 21:11:15 +1200

    test: rimuc tests: throw an error on an illegal test predicate and fix a couple of lint warnings.

--- test/rimuc-tests.js
+++ test/rimuc-tests.js
@@ -2,8 +2,6 @@ const test = require('tape');
 const exec = require('child_process').exec;
 const fs = require('fs');
 
-const test_descriptors = [];
-
 // source: Rimu source.
 // options: rimuc command options.
 // callback(output, error): output=stdout+stderr, error=exit code
@@ -50,8 +48,11 @@ test('rimuc', function (t) {
         break;
       case "exitCode":
         rimuc_exec(e.input, e.args, function (output, error) {
-          t.ok(error.code == e.expectedOutput, e.description)
+          t.ok(String(error.code) === e.expectedOutput, e.description)
         });
+        break;
+      default:
+        t.ok(false, e.description + ': illegal predicate: ' + e.predicate)
         break;
     }
   });
