9378f47bd73c99ef3e58799bcf4e99655c917eca
Stuart Rackham <srackham@gmail.com>
2019-12-15 16:19:40 +1300

    refactor: Move rimuc help text to resource file.

--- src/rimuc/webpack.config.js
+++ src/rimuc/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {test: /\.tsx?$/, loader: 'ts-loader'},
-      {test: /\/resources\/.*\.rmu$/, loader: 'raw-loader'},
+      {test: /\/resources\/.*\.(rmu|txt)$/, loader: 'raw-loader'},
     ]
   }
 };
