7be84e8da11c512f9fe9422ce41898108b9f5910
Stuart Rackham <srackham@gmail.com>
2020-01-10 11:38:05 +1300

    test: Updated validate-rimu-ports.sh to include Python port.

--- test/validate-rimu-ports.sh
+++ test/validate-rimu-ports.sh
@@ -3,7 +3,7 @@
 # USAGE
 #    validate-rimu-ports.sh [--update-fixtures]
 #
-# This script is used to verify all 3 Rimu ports (JS, Go, Kotlin) are congruent.
+# This script is used to verify all Rimu ports are congruent.
 # If any discrepencies are detected it exits immediately.
 # Run this script before publishing a new Rimu release.
 #
@@ -24,6 +24,7 @@ GO=$HOME/local/projects/go-rimu
 JS=$HOME/local/projects/rimu
 KT=$HOME/local/projects/rimu-kt
 DART=$HOME/local/projects/rimu-dart
+PYTHON=$HOME/local/projects/rimu-py
 
 # Copy test fixtures, resources and example rimurc file.
 if [ "${1:-}" = "--update-fixtures" ]; then
@@ -39,11 +40,16 @@ if [ "${1:-}" = "--update-fixtures" ]; then
     cp $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json
     cp $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu
 
+    cp $JS/test/rimu-tests.json $PYTHON/tests/rimu-tests.json
+    cp $JS/test/rimuc-tests.json $PYTHON/tests/rimuc-tests.json
+    cp $JS/src/examples/example-rimurc.rmu $PYTHON/tests/fixtures/example-rimurc.rmu
+
     cd $JS/src/rimuc/resources
     for f in *; do
         cp $f $GO/rimugo/resources/$f
         cp $f $KT/src/main/resources/org/rimumarkup/$f
         cp $f $DART/lib/resources/$f
+        cp $f $PYTHON/src/rimuc/resources/$f
     done
 fi
 
@@ -61,11 +67,16 @@ diff $JS/test/rimu-tests.json $DART/test/rimu-tests.json || err=1
 diff $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json || err=1
 diff $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu || err=1
 
+diff $JS/test/rimu-tests.json $PYTHON/tests/rimu-tests.json || err=1
+diff $JS/test/rimuc-tests.json $PYTHON/tests/rimuc-tests.json || err=1
+diff $JS/src/examples/example-rimurc.rmu $PYTHON/tests/fixtures/example-rimurc.rmu || err=1
+
 cd $JS/src/rimuc/resources
 for f in *; do
     diff $f $GO/rimugo/resources/$f || err=1
     diff $f $KT/src/main/resources/org/rimumarkup/$f || err=1
     diff $f $DART/lib/resources/$f || err=1
+    diff $f $PYTHON/src/rimuc/resources/$f || err=1
 done
 
 if [ $err = 1 ]; then
@@ -85,6 +96,10 @@ cd $KT
 cd $DART
 make
 
+cd $PYTHON
+source .venv/bin/activate
+make clean build install
+
 # Compile Rimu documentation with all ports and compare.
 for doc in reference tips changelog; do
     ARGS='--no-rimurc --theme legend --custom-toc --header-links --layout sequel --lang en --title "Rimu Reference" --highlightjs --prepend "{generate-examples}='"'yes'"'"  ./src/examples/example-rimurc.rmu ./docs/doc-header.rmu'
@@ -92,14 +107,17 @@ for doc in reference tips changelog; do
     JS_DOC=/tmp/$doc-js.html
     KT_DOC=/tmp/$doc-kt.html
     DART_DOC=/tmp/$doc-dart.html
+    PYTHON_DOC=/tmp/$doc-python.html
 
     cd $JS
     eval node bin/rimuc.js --output $JS_DOC $ARGS ./docs/$doc.rmu
     eval rimugo --output $GO_DOC $ARGS ./docs/$doc.rmu
     eval $KT/build/install/rimu-kt/bin/rimukt --output $KT_DOC $ARGS ./docs/$doc.rmu
     eval $DART/build/rimuc --output $DART_DOC $ARGS ./docs/$doc.rmu
+    eval rimupy --output $PYTHON_DOC $ARGS ./docs/$doc.rmu
 
     diff $JS_DOC $GO_DOC
     diff $JS_DOC $KT_DOC
     diff $JS_DOC $DART_DOC
+    diff $JS_DOC $PYTHON_DOC
 done
