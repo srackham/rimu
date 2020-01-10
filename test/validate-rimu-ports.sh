feb6e3bd53b475cf76f1a96f1256b3e598ad79f0
Stuart Rackham <srackham@gmail.com>
2020-01-10 17:01:44 +1300

    test: validate-rimu-ports.sh prints documentation compilation times for each Rimu language port.

--- test/validate-rimu-ports.sh
+++ test/validate-rimu-ports.sh
@@ -17,14 +17,15 @@
 # - Compiles the Rimu documentation with all three ports and checks they are identical.
 #
 
-set -eux
+set -eu
+# set -x
 
 # Project directories.
 GO=$HOME/local/projects/go-rimu
 JS=$HOME/local/projects/rimu
 KT=$HOME/local/projects/rimu-kt
 DART=$HOME/local/projects/rimu-dart
-PYTHON=$HOME/local/projects/rimu-py
+PY=$HOME/local/projects/rimu-py
 
 # Copy test fixtures, resources and example rimurc file.
 if [ "${1:-}" = "--update-fixtures" ]; then
@@ -40,16 +41,16 @@ if [ "${1:-}" = "--update-fixtures" ]; then
     cp $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json
     cp $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu
 
-    cp $JS/test/rimu-tests.json $PYTHON/tests/rimu-tests.json
-    cp $JS/test/rimuc-tests.json $PYTHON/tests/rimuc-tests.json
-    cp $JS/src/examples/example-rimurc.rmu $PYTHON/tests/fixtures/example-rimurc.rmu
+    cp $JS/test/rimu-tests.json $PY/tests/rimu-tests.json
+    cp $JS/test/rimuc-tests.json $PY/tests/rimuc-tests.json
+    cp $JS/src/examples/example-rimurc.rmu $PY/tests/fixtures/example-rimurc.rmu
 
     cd $JS/src/rimuc/resources
     for f in *; do
         cp $f $GO/rimugo/resources/$f
         cp $f $KT/src/main/resources/org/rimumarkup/$f
         cp $f $DART/lib/resources/$f
-        cp $f $PYTHON/src/rimuc/resources/$f
+        cp $f $PY/src/rimuc/resources/$f
     done
 fi
 
@@ -67,16 +68,16 @@ diff $JS/test/rimu-tests.json $DART/test/rimu-tests.json || err=1
 diff $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json || err=1
 diff $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu || err=1
 
-diff $JS/test/rimu-tests.json $PYTHON/tests/rimu-tests.json || err=1
-diff $JS/test/rimuc-tests.json $PYTHON/tests/rimuc-tests.json || err=1
-diff $JS/src/examples/example-rimurc.rmu $PYTHON/tests/fixtures/example-rimurc.rmu || err=1
+diff $JS/test/rimu-tests.json $PY/tests/rimu-tests.json || err=1
+diff $JS/test/rimuc-tests.json $PY/tests/rimuc-tests.json || err=1
+diff $JS/src/examples/example-rimurc.rmu $PY/tests/fixtures/example-rimurc.rmu || err=1
 
 cd $JS/src/rimuc/resources
 for f in *; do
     diff $f $GO/rimugo/resources/$f || err=1
     diff $f $KT/src/main/resources/org/rimumarkup/$f || err=1
     diff $f $DART/lib/resources/$f || err=1
-    diff $f $PYTHON/src/rimuc/resources/$f || err=1
+    diff $f $PY/src/rimuc/resources/$f || err=1
 done
 
 if [ $err = 1 ]; then
@@ -96,28 +97,73 @@ cd $KT
 cd $DART
 make
 
-cd $PYTHON
+cd $PY
 source .venv/bin/activate
 make clean build install
 
 # Compile Rimu documentation with all ports and compare.
+function start() {
+    START_TIME=$(date +%s.%N)
+}
+
+function duration() {
+    echo $(echo "$(date +%s.%N) - $START_TIME + ${1:-0}" | bc)
+}
+
+function seconds() {
+    echo $(printf "%.2f seconds" $1)
+}
+
+JS_TIME=0
+GO_TIME=0
+KT_TIME=0
+DART_TIME=0
+PY_TIME=0
+
+DOC_LINES=$(cat $JS/docs/{reference,tips,changelog}.rmu | wc -l)
+
 for doc in reference tips changelog; do
     ARGS='--no-rimurc --theme legend --custom-toc --header-links --layout sequel --lang en --title "Rimu Reference" --highlightjs --prepend "{generate-examples}='"'yes'"'"  ./src/examples/example-rimurc.rmu ./docs/doc-header.rmu'
     GO_DOC=/tmp/$doc-go.html
     JS_DOC=/tmp/$doc-js.html
     KT_DOC=/tmp/$doc-kt.html
     DART_DOC=/tmp/$doc-dart.html
-    PYTHON_DOC=/tmp/$doc-python.html
+    PY_DOC=/tmp/$doc-python.html
 
     cd $JS
+
+
+    start
     eval node bin/rimuc.js --output $JS_DOC $ARGS ./docs/$doc.rmu
+    JS_TIME=$(duration $JS_TIME)
+
+    start
     eval rimugo --output $GO_DOC $ARGS ./docs/$doc.rmu
+    GO_TIME=$(duration $GO_TIME)
+
+    start
     eval $KT/build/install/rimu-kt/bin/rimukt --output $KT_DOC $ARGS ./docs/$doc.rmu
+    KT_TIME=$(duration $KT_TIME)
+
+    start
     eval $DART/build/rimuc --output $DART_DOC $ARGS ./docs/$doc.rmu
-    eval rimupy --output $PYTHON_DOC $ARGS ./docs/$doc.rmu
+    DART_TIME=$(duration $DART_TIME)
+
+    start
+    eval rimupy --output $PY_DOC $ARGS ./docs/$doc.rmu
+    PY_TIME=$(duration $PY_TIME)
 
     diff $JS_DOC $GO_DOC
     diff $JS_DOC $KT_DOC
     diff $JS_DOC $DART_DOC
-    diff $JS_DOC $PYTHON_DOC
+    diff $JS_DOC $PY_DOC
 done
+
+set +x
+echo
+echo "DOCUMENTATION GENERATION EXECUTION TIMES ($DOC_LINES lines of Rimu markup)"
+echo "JavaScript: $(seconds $JS_TIME)"
+echo "Go:         $(seconds $GO_TIME)"
+echo "Kotlin:     $(seconds $KT_TIME)"
+echo "Dart:       $(seconds $DART_TIME)"
+echo "Python:     $(seconds $PY_TIME)"
