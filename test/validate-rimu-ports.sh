ee21c97081d665a8b26baf715bdbf7ec60f11ffa
Stuart Rackham <srackham@gmail.com>
2020-01-31 17:27:43 +1300

    test: Update validation script to include Rimu Deno port.

--- test/validate-rimu-ports.sh
+++ test/validate-rimu-ports.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #
-# USAGE
-#    validate-rimu-ports.sh [--update-fixtures]
+# USAGE validate-rimu-ports.sh [--update-fixtures] [--skip-fixtures] [--skip-tests]
 #
-# This script is used to verify all Rimu ports are congruent.
-# If any discrepencies are detected it exits immediately.
-# Run this script before publishing a new Rimu release.
+# This script is used to verify all Rimu ports are congruent. If any
+# discrepencies are detected it exits immediately. Run this script before
+# publishing a new Rimu release.
 #
 # NOTE: Assumes the JS port contains the latest versions of common test fixture
 #       and resource files. If Go or Kotlin ports contain latest versions they
 #       should be copied to the JS project before running this script.
 #
-# - If invoked with --update-fixtures argument it copies common test fixtures and resource
-#   files from JS project to Go and Kotlin projects.
-# - Tests and builds all three Rimu ports.
-# - Compiles the Rimu documentation with all three ports and checks they are identical.
+# - If invoked with --update-fixtures argument it copies common test fixtures
+#   and resource files from JS project to the other ports.
+# - If invoked with --skip-fixtures argument the resources and fixtures
+#   comparision tests are not performed.
+# - If invoked with --skip-tests argument the resources and fixtures comparision
+#   and the tests are skipped.
+# - Tests and builds all Rimu ports.
+# - Compiles the Rimu documentation with all three ports and checks they are
+#   identical.
 #
 
 set -eu
@@ -26,9 +30,16 @@ JS=$HOME/local/projects/rimu
 KT=$HOME/local/projects/rimu-kt
 DART=$HOME/local/projects/rimu-dart
 PY=$HOME/local/projects/rimu-py
+DENO=$HOME/local/projects/rimu-deno
+
+# Return 0 exitcode if $2 wholly contained in $1.
+stringContains() {
+    [ -n "$1" -a -z "${1##*$2*}" ];
+}
 
 # Copy test fixtures, resources and example rimurc file.
-if [ "${1:-}" = "--update-fixtures" ]; then
+if stringContains "$*" --update-fixtures; then
+    echo "Updating test fixtures and resources..."
     cp $JS/test/rimu-tests.json $GO/rimu/testdata/rimu-tests.json
     cp $JS/test/rimuc-tests.json $GO/rimugo/testdata/rimuc-tests.json
     cp $JS/src/examples/example-rimurc.rmu $GO/rimugo/testdata/example-rimurc.rmu
@@ -55,51 +66,60 @@ if [ "${1:-}" = "--update-fixtures" ]; then
 fi
 
 # Proceed only if all test fixtures and resource files are identical.
-err=0
-diff $JS/test/rimu-tests.json $GO/rimu/testdata/rimu-tests.json || err=1
-diff $JS/test/rimuc-tests.json $GO/rimugo/testdata/rimuc-tests.json || err=1
-diff $JS/src/examples/example-rimurc.rmu $GO/rimugo/testdata/example-rimurc.rmu || err=1
+if ! stringContains "$*" --skip-fixtures; then
+    echo "Checking test fixtures and resources are up to date..."
+    err=0
+    diff $JS/test/rimu-tests.json $GO/rimu/testdata/rimu-tests.json || err=1
+    diff $JS/test/rimuc-tests.json $GO/rimugo/testdata/rimuc-tests.json || err=1
+    diff $JS/src/examples/example-rimurc.rmu $GO/rimugo/testdata/example-rimurc.rmu || err=1
 
-diff $JS/test/rimu-tests.json $KT/src/test/resources/rimu-tests.json || err=1
-diff $JS/test/rimuc-tests.json $KT/src/test/resources/rimuc-tests.json || err=1
-diff $JS/src/examples/example-rimurc.rmu $KT/src/test/fixtures/example-rimurc.rmu || err=1
+    diff $JS/test/rimu-tests.json $KT/src/test/resources/rimu-tests.json || err=1
+    diff $JS/test/rimuc-tests.json $KT/src/test/resources/rimuc-tests.json || err=1
+    diff $JS/src/examples/example-rimurc.rmu $KT/src/test/fixtures/example-rimurc.rmu || err=1
 
-diff $JS/test/rimu-tests.json $DART/test/rimu-tests.json || err=1
-diff $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json || err=1
-diff $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu || err=1
+    diff $JS/test/rimu-tests.json $DART/test/rimu-tests.json || err=1
+    diff $JS/test/rimuc-tests.json $DART/test/rimuc-tests.json || err=1
+    diff $JS/src/examples/example-rimurc.rmu $DART/test/fixtures/example-rimurc.rmu || err=1
 
-diff $JS/test/rimu-tests.json $PY/tests/rimu-tests.json || err=1
-diff $JS/test/rimuc-tests.json $PY/tests/rimuc-tests.json || err=1
-diff $JS/src/examples/example-rimurc.rmu $PY/tests/fixtures/example-rimurc.rmu || err=1
+    diff $JS/test/rimu-tests.json $PY/tests/rimu-tests.json || err=1
+    diff $JS/test/rimuc-tests.json $PY/tests/rimuc-tests.json || err=1
+    diff $JS/src/examples/example-rimurc.rmu $PY/tests/fixtures/example-rimurc.rmu || err=1
 
-cd $JS/src/rimuc/resources
-for f in *; do
-    diff $f $GO/rimugo/resources/$f || err=1
-    diff $f $KT/src/main/resources/org/rimumarkup/$f || err=1
-    diff $f $DART/lib/resources/$f || err=1
-    diff $f $PY/src/rimuc/resources/$f || err=1
-done
+    cd $JS/src/rimuc/resources
+    for f in *; do
+        diff $f $GO/rimugo/resources/$f || err=1
+        diff $f $KT/src/main/resources/org/rimumarkup/$f || err=1
+        diff $f $DART/lib/resources/$f || err=1
+        diff $f $PY/src/rimuc/resources/$f || err=1
+    done
 
-if [ $err = 1 ]; then
-    exit 1
+    if [ $err = 1 ]; then
+        exit 1
+    fi
 fi
 
 # Build and test all ports.
-cd $JS
-jake test
+if ! stringContains "$*" --skip-tests; then
+    echo "Running tests..."
+    cd $JS
+    jake test
 
-cd $GO
-make
+    cd $GO
+    make
 
-cd $KT
-./gradlew --console plain test installDist
+    cd $KT
+    ./gradlew --console plain test installDist
 
-cd $DART
-make
+    cd $DART
+    make
 
-cd $PY
-source .venv/bin/activate
-make clean build install
+    cd $PY
+    source .venv/bin/activate
+    make clean build install
+
+    cd $DENO
+    make
+fi
 
 # Compile Rimu documentation with all ports and compare.
 function start() {
@@ -119,9 +139,11 @@ GO_TIME=0
 KT_TIME=0
 DART_TIME=0
 PY_TIME=0
+DENO_TIME=0
 
 DOC_LINES=$(cat $JS/docs/{doc-header,reference,tips,changelog,manpage}.rmu $JS/src/examples/example-rimurc.rmu | wc -l)
 
+echo "Compiling and comparing documentation..."
 for doc in reference tips changelog; do
     ARGS='--no-rimurc --theme legend --custom-toc --header-links --layout sequel --lang en --title "Rimu Reference" --highlightjs --prepend "{generate-examples}='"'yes'"'"  ./src/examples/example-rimurc.rmu ./docs/manpage.rmu ./docs/doc-header.rmu'
     GO_DOC=/tmp/$doc-go.html
@@ -129,6 +151,7 @@ for doc in reference tips changelog; do
     KT_DOC=/tmp/$doc-kt.html
     DART_DOC=/tmp/$doc-dart.html
     PY_DOC=/tmp/$doc-python.html
+    DENO_DOC=/tmp/$doc-deno.html
 
     cd $JS
 
@@ -153,10 +176,17 @@ for doc in reference tips changelog; do
     eval rimupy --output $PY_DOC $ARGS ./docs/$doc.rmu
     PY_TIME=$(duration $PY_TIME)
 
+    start
+    eval deno --allow-env --allow-read --allow-write $DENO/src/rimuc.ts --output $DENO_DOC $ARGS ./docs/$doc.rmu
+    # Deno install bash wrapper adds an extra shell invocation.
+    # eval rimudeno --output $DENO_DOC $ARGS ./docs/$doc.rmu
+    DENO_TIME=$(duration $DENO_TIME)
+
     diff $JS_DOC $GO_DOC
     diff $JS_DOC $KT_DOC
     diff $JS_DOC $DART_DOC
     diff $JS_DOC $PY_DOC
+    diff $JS_DOC $DENO_DOC
 done
 
 set +x
@@ -167,3 +197,4 @@ echo "Go:         $(seconds $GO_TIME)"
 echo "Kotlin:     $(seconds $KT_TIME)"
 echo "Dart:       $(seconds $DART_TIME)"
 echo "Python:     $(seconds $PY_TIME)"
+echo "Deno:       $(seconds $DENO_TIME)"
