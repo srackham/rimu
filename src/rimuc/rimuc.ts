01e6f1d405afdb049fef64f2659fbca129e68718
Stuart Rackham <srackham@gmail.com>
2020-03-17 19:03:16 +1300

    Bump version number.

--- src/rimuc/rimuc.ts
+++ src/rimuc/rimuc.ts
@@ -8,7 +8,7 @@ import * as path from 'path'
 import * as rimu from 'rimu'
 import { resources } from './resources'
 
-const VERSION = '11.1.6'
+const VERSION = '11.1.7'
 const STDIN = '/dev/stdin'
 const HOME_DIR = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']
 const RIMURC = path.resolve(HOME_DIR || '', '.rimurc')
