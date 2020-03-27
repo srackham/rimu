608ac48b8ea124a5ee7c4b7ea06d6022738be1dc
Stuart Rackham <srackham@gmail.com>
2020-03-28 10:22:25 +1300

    docs: Added and edited examples.

---
+++ src/examples/deno-minimal-example.ts
@@ -0,0 +1,5 @@
+// Minimal Rimu Deno example.
+
+import * as rimu from "https://raw.github.com/srackham/rimu/master/mod.ts";
+
+console.log(rimu.render("Hello *Rimu*!"));
---
+++ src/examples/nodejs-minimal-example.ts
@@ -0,0 +1,5 @@
+// Minimal Rimu Node.js example.
+
+import * as rimu from "rimu";
+
+console.log(rimu.render("Hello *Rimu*!", { safeMode: 1 }));
