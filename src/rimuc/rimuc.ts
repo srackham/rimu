5c8d832c8fb5dd041a22534d0a0733cfb5b42ddf
Stuart Rackham <srackham@gmail.com>
2020-05-16 14:58:51 +1200

    Bump version number.

--- src/rimuc/rimuc.ts
+++ src/rimuc/rimuc.ts
@@ -8,7 +8,7 @@ import * as path from "path";
 import * as rimu from "rimu";
 import { resources } from "./resources";
 
-const VERSION = "11.1.8";
+const VERSION = '11.1.9';
 const STDIN = "/dev/stdin";
 const HOME_DIR =
   process.env[(process.platform === "win32") ? "USERPROFILE" : "HOME"];
