62464eea66c15f5d07bc94a76a8e0a1250a80fbe
Stuart Rackham <srackham@gmail.com>
2020-05-30 15:40:45 +1200

    build: Pinned Deno libs: std@v0.54.0.

--- test/deps.ts
+++ test/deps.ts
@@ -1,11 +1,12 @@
 export {
-  assert,
-  assertEquals,
-  assertNotEquals,
-} from "https://deno.land/std@v0.52.0/testing/asserts.ts";
-export {
   env,
   readFile,
   shCapture,
   ShOutput,
-} from "https://deno.land/x/drake@v1.1.1/lib.ts";
+} from "file:///home/srackham/local/projects/drake/lib.ts";
+export {
+  assert,
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std@v0.54.0/testing/asserts.ts";
+// } from "https://deno.land/x/drake@v1.1.1/lib.ts";
