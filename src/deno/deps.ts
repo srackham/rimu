8e229af123ca2978780de9b6a0a541ea49c99580
Stuart Rackham <srackham@gmail.com>
2020-08-23 08:45:49 +1200

    upgrade: Pinned Deno libs: std@0.66.0.

--- src/deno/deps.ts
+++ src/deno/deps.ts
@@ -1,2 +1,2 @@
-export { existsSync } from "https://deno.land/std@v0.65.0/fs/exists.ts";
-export * as path from "https://deno.land/std@v0.65.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.66.0/fs/exists.ts";
+export * as path from "https://deno.land/std@0.66.0/path/mod.ts";
