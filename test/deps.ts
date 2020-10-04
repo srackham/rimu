557b89c3af4a3a2774d3885d2160b716dfc2a0d1
Stuart Rackham <srackham@gmail.com>
2020-10-04 13:21:10 +1300

    test: update to Drake 1.4.3

--- test/deps.ts
+++ test/deps.ts
@@ -4,10 +4,8 @@ export {
   assertNotEquals,
 } from "https://deno.land/std@0.73.0/testing/asserts.ts";
 export {
-  // TODO: Export env from lib in Drake >1.4.2
-  // env,
+  env,
   readFile,
   shCapture,
   ShOutput,
 } from "https://deno.land/x/drake@v1.4.3/lib.ts";
-export { env } from "https://deno.land/x/drake@v1.4.3/mod.ts";
