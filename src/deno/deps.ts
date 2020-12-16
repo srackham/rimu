cd67da0ea9f312cf32a3a189c0bbc1f53c959024
Stuart Rackham <srackham@gmail.com>
2020-12-17 07:56:34 +1300

    build: update Deno libs to std@v0.81.0

--- src/deno/deps.ts
+++ src/deno/deps.ts
@@ -1,2 +1,2 @@
-export { existsSync } from "https://deno.land/std@0.78.0/fs/exists.ts";
-export * as path from "https://deno.land/std@0.78.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.81.0/fs/exists.ts";
+export * as path from "https://deno.land/std@0.81.0/path/mod.ts";
--- test/deps.ts
+++ test/deps.ts
@@ -2,7 +2,7 @@ export {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.78.0/testing/asserts.ts";
+} from "https://deno.land/std@0.81.0/testing/asserts.ts";
 export {
   env,
   readFile,
