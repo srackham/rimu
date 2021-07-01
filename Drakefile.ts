031bbabcd2f47ccbc386982148b997f7a2a4443b
Stuart Rackham <srackham@gmail.com>
2021-07-02 09:29:14 +1200

    build: update Deno libs to std@v0.100.0

--- Drakefile.ts
+++ Drakefile.ts
@@ -4,7 +4,7 @@
  * Drakefile for Rimu Markup (http://github.com/srackham/rimu).
  */
 
-import * as path from "https://deno.land/std@0.99.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.100.0/path/mod.ts";
 import {
   abort,
   desc,
--- test/deps.ts
+++ test/deps.ts
@@ -2,7 +2,7 @@ export {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.99.0/testing/asserts.ts";
+} from "https://deno.land/std@0.100.0/testing/asserts.ts";
 export {
   env,
   readFile,
