7da862768fa23feb321237854683470d31ae7ae8
Stuart Rackham <srackham@gmail.com>
2021-08-02 11:58:36 +1200

    build: update Deno libs to std@v0.103.0

--- src/deno/deps.ts
+++ src/deno/deps.ts
@@ -1,3 +1,3 @@
-export { existsSync } from "https://deno.land/std@0.100.0/fs/exists.ts";
-export { readAll } from "https://deno.land/std@0.100.0/io/util.ts";
-export * as path from "https://deno.land/std@0.100.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.103.0/fs/exists.ts";
+export { readAll } from "https://deno.land/std@0.103.0/io/util.ts";
+export * as path from "https://deno.land/std@0.103.0/path/mod.ts";
