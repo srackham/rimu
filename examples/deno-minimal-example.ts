86cbadb92b7f0307dd7cf7f8c5f98b862c9df461
Stuart Rackham <srackham@gmail.com>
2022-06-07 16:48:22 +1200

    docs: update examples with latest Rimu version number

--- examples/deno-minimal-example.ts
+++ examples/deno-minimal-example.ts
@@ -1,5 +1,5 @@
 // Minimal Rimu Deno example.
 
-import * as rimu from "https://deno.land/x/rimu@11.2.0/mod.ts";
+import * as rimu from "https://deno.land/x/rimu@11.3.0/mod.ts";
 
 console.log(rimu.render("Hello *Rimu*!"));
