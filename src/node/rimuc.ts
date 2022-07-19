8f15dccffdecdef36afbc17bd4a776bcb13a5ccc
Stuart Rackham <srackham@gmail.com>
2022-07-20 07:38:58 +1200

    chore: bump version number

--- src/node/rimuc.ts
+++ src/node/rimuc.ts
@@ -8,7 +8,7 @@ import * as path from "path";
 import * as rimu from "./rimu";
 import { resources } from "./resources";
 
-const VERSION = "11.3.0";
+const VERSION = "11.4.0";
 const STDIN = "/dev/stdin";
 const HOME_DIR =
   process.env[(process.platform === "win32") ? "USERPROFILE" : "HOME"];
