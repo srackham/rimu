9dc7c9e4804af36a82fa873216beeb00668b005c
Stuart Rackham <srackham@gmail.com>
2023-01-22 08:31:44 +1300

    build: update Deno libs to std@v0.173.0, Deno 1.29.4

--- src/deno/deps.ts
+++ src/deno/deps.ts
@@ -1,2 +1,2 @@
-export * as path from "https://deno.land/std@0.155.0/path/mod.ts";
-export { readAll } from "https://deno.land/std@0.155.0/streams/conversion.ts";
+export * as path from "https://deno.land/std@0.173.0/path/mod.ts";
+export { readAll } from "https://deno.land/std@0.173.0/streams/mod.ts";
